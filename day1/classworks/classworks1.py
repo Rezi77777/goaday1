f6db1b06b5d36255750e24c14f0db075d1267a23
Revazi Neparidze <145490505+Rezi77777@users.noreply.github.com>
2023-09-28 23:08:35 +0400

    Add files via upload

---
+++ day1/classworks/classworks1.py
@@ -0,0 +1,8 @@
+mom_age=45
+dad_age=61
+my_age=36
+
+
+mom_age=mom_age+5
+dad_age=dad_age+5
+my_age=my_age+5
---
+++ day1/homeworks/homeworks1.py
@@ -0,0 +1,52 @@
+print("hi")
+print("big house")
+print("vagetables")
+print("walk right")
+
+
+print(20)
+print(8+8)
+print("5"+"6"+"10")
+
+item="phone"
+price=1500
+print(price)
+
+print(55+7)
+print(100-8)
+print(147*7)
+print(70 / 4)
+print(70 // 4)
+
+
+balance=1450
+print(balance)
+
+score=98
+print(score)
+
+
+
+name="rezi"
+age=36
+city="tbilisi"
+print(name)
+print(age)
+
+
+
+balance=20
+print(balance+20)
+
+
+price=5
+amount=8
+print(price+amount)
+
+budget=20
+print(budget+50)
+
+
+price=50
+discount=0.5*price
+print(discount)
